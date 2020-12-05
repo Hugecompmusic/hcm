7dc1b1171acdcaa1025e0e2f47811dbbcf16881a
Hugecompmusic <74491974+Hugecompmusic@users.noreply.github.com>
2020-12-05 19:52:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,13 +23,11 @@ client.on("ready", () => {
     ",play", 
     ",help",
     ",invite", 
-    "HugeCompMusic"
-    ]; // creates an arraylist containing phrases you want your bot to switch through.
-
+    "HugeCompMusic"]; 
 bot.on('ready', () => {
     setInterval(() => {
-        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
-        bot.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
+        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1);
+        bot.user.setActivity(activities_list[index]); 
     }, 10000); // Runs this every 10 seconds.
 });
 client.on("warn", (info) => console.log(info));
