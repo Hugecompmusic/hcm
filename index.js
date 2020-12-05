36ae22c5abc0810cfd46d0c7d9f61291871e51db
Hugecompmusic <74491974+Hugecompmusic@users.noreply.github.com>
2020-12-05 19:19:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 client.on("ready", () => {
   console.log(`${client.user.username} ready!`);
   client.user.setActivity(`${PREFIX}help and ${PREFIX}play`, { type: "LISTENING" });
+  client.user.setActivity(`MY CREATOR DIE..:(`, { type: "WATCHING" });
 });
 client.on("warn", (info) => console.log(info));
 client.on("error", console.error);
