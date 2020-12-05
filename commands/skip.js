14663c8ec9fbbf4cd3d144eb556bca3c59a7af20
Hugecompmusic <74491974+Hugecompmusic@users.noreply.github.com>
2020-12-05 19:03:07 +0530

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -12,6 +12,6 @@ module.exports = {
 
     queue.playing = true;
     queue.connection.dispatcher.end();
-    queue.textChannel.send(`${message.author} ⏭ skipped the song`).catch(console.error);
+    queue.textChannel.send(`${message.author} ⏭ skipped the song idk why!`).catch(console.error);
   }
 };
