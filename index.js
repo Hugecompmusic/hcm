1e16ebbc226054b880d1ad867073b72cef0e1e22
Hugecompmusic <74491974+Hugecompmusic@users.noreply.github.com>
2020-12-05 19:46:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,9 +19,18 @@ const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
  * Client Events
  */
 client.on("ready", () => {
-  console.log(`${client.user.username} ready!`);
-  client.user.setActivity(`${PREFIX}help and ${PREFIX}play`, { type: "LISTENING" });
-  client.user.setActivity(`MY CREATOR DIE..:(`, { type: "WATCHING" });
+  const activities_list = [
+    ",play", 
+    ",help",
+    ",invite", 
+    "HugeCompMusic"
+    ]; // creates an arraylist containing phrases you want your bot to switch through.
+
+bot.on('ready', () => {
+    setInterval(() => {
+        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
+        bot.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
+    }, 10000); // Runs this every 10 seconds.
 });
 client.on("warn", (info) => console.log(info));
 client.on("error", console.error);
