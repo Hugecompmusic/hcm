3a51e19685dfc0fb71034793fbf76320cbf9c981
Hugecompmusic <74491974+Hugecompmusic@users.noreply.github.com>
2020-12-06 19:42:20 +0530

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -7,6 +7,7 @@ module.exports = {
     let minutes = Math.floor(seconds / 60);
     let hours = Math.floor(minutes / 60);
     let days = Math.floor(hours / 24);
+    
 
     seconds %= 60;
     minutes %= 60;
@@ -15,5 +16,21 @@ module.exports = {
     return message
       .reply(`Uptime: \`${days} day(s),${hours} hours, ${minutes} minutes, ${seconds} seconds\``)
       .catch(console.error);
+    
+    
+     let embed = new MessageEmbed()
+    .setColor(COLOR)
+    .setThumbnail(client.user.displayAvatarURL())
+    .setAuthor(`STATS AND INFORMATION`, client.user.displayAvatarURL())
+    .setDescription(`My name is **${client.user.username}** and My work is to play Music`)
+    .addField("SERVERS", client.guilds.cache.size, true)
+    .addField("PRESENCE", client.user.presence.activities[0].name, true)
+    .addField("ID", client.user.id, true)
+    .addField("UPTIME", ms(client.uptime), true)
+    .addField("STATUS", client.user.presence.status, true)
+    .addField("TOTAL MEMBERS", client.users.cache.size, true)
+    .addField("NO. OF GUILD IN BOT IS PLAYING", client.queue.size)
+ console.log(client.user.presence)
+    message.channel.send(embed)
   }
 };
