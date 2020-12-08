95138fccdad32f77085542641e2a7814e541f76b
Hugecompmusic <74491974+Hugecompmusic@users.noreply.github.com>
2020-12-08 16:59:43 +0530

    Create serverinfo.js

---
+++ commands/serverinfo.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    function checkDays(date) {
+        let now = new Date();
+        let diff = now.getTime() - date.getTime();
+        let days = Math.floor(diff / 86400000);
+        return days + (days == 1 ? " day" : " days") + " ago";
+    };
+    let verifLevels = ["None", "Low", "Medium", "(╯°□°）╯︵  ┻━┻", "┻━┻ミヽ(ಠ益ಠ)ノ彡┻━┻"];
+    let region = {
