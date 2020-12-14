498112c68b58afc3666ffce28e632f5336a9d9db
Hugecompmusic <74491974+Hugecompmusic@users.noreply.github.com>
2020-12-14 20:32:50 +0530

    Update lyrics.js

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -7,15 +7,15 @@ module.exports = {
   description: "Get lyrics for the currently playing song",
   async execute(message) {
     const queue = message.client.queue.get(message.guild.id);
-    if (!queue) return message.channel.send("There is nothing playing.").catch(console.error);
+    if (!queue) return message.channel.send(":)").catch(console.error);
 
     let lyrics = null;
 
     try {
       lyrics = await lyricsFinder(queue.songs[0].title, "");
-      if (!lyrics) lyrics = `No lyrics found for ${queue.songs[0].title}.`;
+      if (!lyrics) lyrics = `:) ${queue.songs[0].title}.`;
     } catch (error) {
-      lyrics = `No lyrics found for ${queue.songs[0].title}.`;
+      lyrics = `:) ${queue.songs[0].title}.`;
     }
 
     let lyricsEmbed = new MessageEmbed()
