14060ac23132e9a410f9e7a9b277df241b6864e5
Hugecompmusic <74491974+Hugecompmusic@users.noreply.github.com>
2020-12-22 12:18:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
  */
 client.on("ready", () => {
   console.log(`${client.user.username} ready!`);
-  client.user.setActivity(`${PREFIX}help and ${PREFIX}play`, { type: "STREAMING" }, status: 'idle' });
+  client.user.setActivity(`${PREFIX}help and ${PREFIX}play`, { type: "STREAMING" }, {status: "idle" }});
  
 });
 client.on("warn", (info) => console.log(info));
