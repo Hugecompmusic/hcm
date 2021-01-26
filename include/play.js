93904c96a59802368de7b21d3870b311f11bdb7f
Hugecompmusic <74491974+Hugecompmusic@users.noreply.github.com>
2021-01-26 14:32:13 +0530

    Update play.js

--- include/play.js
+++ include/play.js
@@ -79,14 +79,6 @@ module.exports = {
     dispatcher.setVolumeLogarithmic(queue.volume / 100);
 
     try {
-         const newsong = new MessageEmbed()
-        .setTitle("<:Playing:769665713124016128>  "+song.title)
-        .setURL(song.url)
-        .setColor("#c219d8")
-        .setThumbnail(thumb)
-        .setFooter(`Requested by: ${message.author.username}#${message.author.discriminator} v2.08.beta`, message.member.user.displayAvatarURL({ dynamic: true }))
-        .addField("Duration:", `\`${song.duration} Minutes\``, true)
-         
       var playingMessage = await queue.textChannel.send(`🎶 Started playing: **${song.title}** ${song.url}`);
       await playingMessage.react("⏭");
       await playingMessage.react("⏯");
